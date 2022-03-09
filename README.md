2c6f48930653897dbe7ee85e23d7872b726de5db
xsd020 <devstrach03@live.com>
2022-03-09 00:59:49 -0500

    Update statusbar.sh to display keyboard layout and reflect change in README.md and example.png

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # statusbar
-Simple status bar script using xsetroot, originally intended for dwm.
+Simple outline for a status bar script using xsetroot, originally intended for dwm.
 
 Currently displays the following info:
 * Volume percentage and mute status
 * Battery percentage
+* Current keyboard layout
 * Date and time
 
 Screenshot:
--- statusbar.sh
+++ statusbar.sh
@@ -6,6 +6,20 @@ status() {
     # Battery %
     echo "B:"$(acpi -b | grep -o '[0-9]*%')
 
+    # Current keyboard layout
+
+    layoutValue=$(xset -q | grep LED | awk '{ print $10 }')
+
+    case $layoutValue in
+	00000000|00000001) layout="en" ;;
+#	00000001) layout="EN" ;;
+	00001000|00001001) layout="fr" ;;
+#	00001001) layout="FR" ;;
+	*) layout = "??" ;;
+    esac
+
+    echo $layout
+
     # Date & time
     date '+%Y %a %d %b %I:%M%p'
     
